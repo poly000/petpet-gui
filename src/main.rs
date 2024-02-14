e4b9140521f424bd7987b6dfca2c88df1ceca5cc
poly000 <1348292515@qq.com>
2024-02-14 14:42:03 +0800

    feat: support APNG encoding

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use petpet::{encode_gif, generate};
+use petpet::{encode_apng, encode_gif, generate};
 use std::io::BufWriter;
 use std::process;
 
 use rfd::FileDialog;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let input_file = FileDialog::new()
         .add_filter(
             "Origin Image",
@@ -14,20 +14,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap_or_else(|| process::exit(0));
 
     let output_file = FileDialog::new()
-        .add_filter("", &["gif"])
+        .add_filter("", &["gif", "apng"])
         .save_file()
         .unwrap_or_else(|| process::exit(0));
 
-    let output = std::fs::File::create(output_file)?;
+    let output = std::fs::File::create(&output_file)?;
 
-    encode_gif(
-        generate(
-            petpet::image::open(input_file)?.to_rgba8(),
-            petpet::FilterType::Lanczos3,
-        )?,
-        BufWriter::new(output),
-        1,
+    let frames = generate(
+        petpet::image::open(input_file)?.to_rgba8(),
+        petpet::FilterType::Lanczos3,
+        None,
     )?;
+
+    if output_file
+        .file_name()
+        .map(|file_name| file_name.to_string_lossy())
+        .is_some_and(|file_name| file_name.ends_with(".apng"))
+    {
+        let delay_num = 1;
+        let delay_den = 50;
+        encode_apng(
+            frames,
+            output,
+            petpet::png::FilterType::NoFilter,
+            delay_num,
+            delay_den,
+        )?;
+    } else {
+        encode_gif(frames, BufWriter::new(output), 1)?;
+    }
 
     Ok(())
 }
